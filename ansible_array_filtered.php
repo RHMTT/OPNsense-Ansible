8f95267fe0eb3f496f50002c9d35360afc2486c7
GoKEV <kev@gokev.com>
2020-08-09 19:51:54 -0500

    Update ansible_array_filtered.php

--- ansible_array_filtered.php
+++ ansible_array_filtered.php
@@ -1,9 +1,26 @@
 <?php
 
+/*
+Some files to check out:
+
+/var/unbound/dhcpleases.conf	unbound leases
+/usr/local/etc/dnsmasq.conf	dnsmasq config (no idea where the leases file is stored since I'm not using it)
+	the option dhcp-leasefile=<lease file>. E.g. dhcp-leasefile=/var/lib/dnsmasq/dnsmasq.leases
+
+
+
+*/
+
 $json = file_get_contents('./array_short.json', true);
 $dhcp = json_decode($json, true);
+$output = ($_REQUEST['output']);
 
-$filters = json_decode($_GET['filter'], true);
+if (isset($_GET['filter'])){
+	$textfilter = $_REQUEST['filter'];
+	$urlfilter = urlencode($_REQUEST['filter']);
+	$urlfilter = urlencode($_REQUEST['filter']);
+	$filters = json_decode($_REQUEST['filter'], true);
+}
 
 /*
 http://10.200.8.117/?filter=[
@@ -47,7 +64,8 @@ foreach( $newarray as $id => $values){
 	$groupct = 0;
         foreach( $values as $var => $val){
 		foreach($filters as $filter){
-			if ( isset($filter['default']) and ! isset($default_group)){
+//			if ( isset($filter['default']) and ! isset($default_group)){
+			if ( isset($filter['default'])){
 				$default_group = $filter['default'];
 			}else{
 				$field = $filter['field'];
@@ -70,10 +88,42 @@ foreach( $newarray as $id => $values){
 }
 
 
-print "<pre>\n";
-print_r($newarray);
-print "</pre>\n";
+if ($output == "json"){
+	header('Content-Type: application/json');
+	$results = json_encode($newarray,JSON_PRETTY_PRINT);
+}elseif ($output == "dict"){
+	header('Content-Type: text/plain');
+	$results = "opnsense_leases:\n";
+	foreach ($newarray as $data) {
+		$results .= "  - mac: " . preg_replace("/:/","-",$data['mac']) . "\n";
+		$results .="    vendor: " . $data['vendor'] . "\n";
+		$results .="    ip: " . $data['ip'] . "\n";
+		$results .="    hostname: " . $data['hostname'] . "\n";
+		$results .="    description: " . $data['descr'] . "\n";
+		$results .="    groups: \n";
+
+		foreach ($data['groups'] as $group){
+			$results .="      - $group" . "\n";
+		}
+
+
+	}
+
+}else{
+	$json_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . "/" . $_SERVER['SCRIPT_NAME'] . "?output=json&filter=" . $urlfilter;
+	$dict_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . "/" . $_SERVER['SCRIPT_NAME'] . "?output=dict&filter=" . $urlfilter;
+
+	$results=<<<ALLDONE
+<font face="verdana,sans,arial">
+<textarea cols="100" rows="10">$textfilter</textarea><br>
+<a href="$json_url">JSON</a> output<br>
+<a href="$dict_url">DICT</a> output<br>
+		This
+
+ALLDONE;
+	$results .= "<pre>" . print_r($newarray, true) . "</pre>";
+}
 
 
 ?>
-
+<?=$results?>
